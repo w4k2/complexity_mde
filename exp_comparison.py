1d1bed9d5ead5e9715e25a7fa38a3633b4cd29ed
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2025-04-29 10:40:58 +0200

    ABC

--- exp_comparison.py
+++ exp_comparison.py
@@ -16,7 +16,7 @@ from utils import Data
 
 
 dataset_names = ['australian', 'banknote', 'breastcancoimbra', 'cryotherapy', 'german', 'haberman', 'heart', 'ionosphere', 'liver', 'mammographic', 'monk-2', 'monkone', 'phoneme', 'pima', 'ring', 'sonar', 'spambase', 'titanic', 'twonorm', 'wisconsin']
-# dataset_names = ['australian']
+# dataset_names = ['australian', 'banknote']
 # DATASET x FOLDS x TRANSFER
 scores = np.load("results/transfer/di_bac_full.npy")
 transrates = np.load("results/transfer/di_transrates_full.npy")
@@ -65,100 +65,103 @@ transfer_names = np.concatenate((np.array(["imagenet" for i in range(best_model_
 # Results
 # DATASETS x FOLDS x TRANSFER (imagenet | best BAC | best transrate) x EPOCH
 n_epochs = 50
-results = np.zeros((len(datasets), 10, 3, n_epochs))
+# results = np.zeros((len(datasets), 10, 3, n_epochs))
+results = np.load("results/transfer/comparison_imgnet_finetuning.npy")
 
 for data_id, dataset_name in enumerate(tqdm(datasets)):
-    if dataset_name == "australian" or dataset_name == "banknote":
-        pass
-    else:
-        # print(dataset_name)
-        X, y = datasets[dataset_name][0], datasets[dataset_name][1]
-        
-        rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=1410)
-        
-        for fold_id, (train_index, test_index) in enumerate(tqdm(rskf.split(X, y), leave=False, desc=f"{data_id}", total=10)):
-            # Encoding        
-            # Train
-            # stml = STML()
-            ln = Norm2Scaler()
-            di = DeepInsight(feature_extractor='pca', 
-            discretization='bin', pixels=(224, 224))
+    if data_name == "breastcancoimbra":
+        exit()
 
-            X_train = X[train_index]
-            X_test = X[test_index]
+    # print(dataset_name)
+    X, y = datasets[dataset_name][0], datasets[dataset_name][1]
+    
+    rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=1410)
+    
+    for fold_id, (train_index, test_index) in enumerate(tqdm(rskf.split(X, y), leave=False, desc=f"{data_id}", total=10)):
+        # Encoding        
+        # Train
+        # stml = STML()
+        ln = Norm2Scaler()
+        di = DeepInsight(feature_extractor='pca', 
+        discretization='bin', pixels=(224, 224))
 
-            X_train = ln.fit_transform(X_train)
-            X_encoded_train = di.fit_transform((X_train))
-            X_encoded_train = torch.from_numpy(np.moveaxis(X_encoded_train, 3, 1)).float()
-            y_train = torch.from_numpy(y[train_index]).long()
-            
-            X_test = ln.transform(X_test)
-            X_encoded_test = di.transform((X_test))
-            X_encoded_test = torch.from_numpy(np.moveaxis(X_encoded_test, 3, 1)).float()
-            y_test = torch.from_numpy(y[test_index]).long()
-            
-            for transfer_id, transfer in enumerate(transfer_names[data_id]):
-                if transfer_id == 2 and transfer_names[data_id][2] == transfer_names[data_id][1]:
-                    pass
+        X_train = X[train_index]
+        X_test = X[test_index]
+
+        X_train = ln.fit_transform(X_train)
+        X_encoded_train = di.fit_transform((X_train))
+        X_encoded_train = torch.from_numpy(np.moveaxis(X_encoded_train, 3, 1)).float()
+        y_train = torch.from_numpy(y[train_index]).long()
+        
+        X_test = ln.transform(X_test)
+        X_encoded_test = di.transform((X_test))
+        X_encoded_test = torch.from_numpy(np.moveaxis(X_encoded_test, 3, 1)).float()
+        y_test = torch.from_numpy(y[test_index]).long()
+        
+        for transfer_id, transfer in enumerate(transfer_names[data_id]):
+            if transfer_id == 2 and transfer_names[data_id][2] == transfer_names[data_id][1]:
+                pass
+            else:
+                # Model
+                num_classes = 2
+                batch_size = 8
+                
+
+                if transfer == "imagenet":
+                    model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+                    # model = resnet18(weights=None)
                 else:
-                    # Model
-                    num_classes = 2
-                    batch_size = 8
+                    model = torch.load("models/model_%s_di.pt" % transfer, weights_only=False)
+                    # model = torch.load("models/model_%s_di_wo_imgnet.pt" % transfer, weights_only=False)
+
+                # Extraction or Fine-tuning
+                # for param in model.parameters():
+                #     param.requires_grad = False
+
+                num_ftrs = model.fc.in_features
+                model.fc = nn.Linear(num_ftrs, num_classes)
+
+                device = torch.device("mps")
+                model = model.to(device)
+
+                """
+                Train
+                """
+
+                optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+                criterion = nn.CrossEntropyLoss()
+                
+                train_dataset = TensorDataset(X_encoded_train, y_train)
+                train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+                
+                for epoch in tqdm(range(n_epochs), leave=False, desc=f"{fold_id}"):
+                    model.train()
+
+                    # print(model.bn1.weight)
+                    # exit()
                     
+                    for i, batch in enumerate(train_data_loader, 0):
+                        inputs, labels = batch
 
-                    if transfer == "imagenet":
-                        model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
-                    else:
-                        model = torch.load("models/model_%s_di.pt" % transfer, weights_only=False)
+                        optimizer.zero_grad()
 
-                    # Extraction or Fine-tuning
-                    # for param in model.parameters():
-                    #     param.requires_grad = False
-
-                    num_ftrs = model.fc.in_features
-                    model.fc = nn.Linear(num_ftrs, num_classes)
-
-                    device = torch.device("mps")
-                    model = model.to(device)
+                        outputs = model(inputs.to(device))
+                        loss = criterion(outputs.to(device), labels.to(device))
+                        loss.backward()
+                        optimizer.step()
 
                     """
-                    Train
+                    Test
                     """
 
-                    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-                    criterion = nn.CrossEntropyLoss()
+                    model.eval()
+
+                    # Results
+                    logits = model(X_encoded_test.to(device))
+                    probs = torch.nn.functional.softmax(logits, dim=1).cpu().detach().numpy()
+                    preds = np.argmax(probs, 1)
                     
-                    train_dataset = TensorDataset(X_encoded_train, y_train)
-                    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-                    
-                    for epoch in tqdm(range(n_epochs), leave=False, desc=f"{fold_id}"):
-                        model.train()
+                    results[data_id, fold_id, transfer_id, epoch] = balanced_accuracy_score(y_test, preds)
 
-                        # print(model.bn1.weight)
-                        # exit()
-                        
-                        for i, batch in enumerate(train_data_loader, 0):
-                            inputs, labels = batch
-
-                            optimizer.zero_grad()
-
-                            outputs = model(inputs.to(device))
-                            loss = criterion(outputs.to(device), labels.to(device))
-                            loss.backward()
-                            optimizer.step()
-
-                        """
-                        Test
-                        """
-
-                        model.eval()
-
-                        # Results
-                        logits = model(X_encoded_test.to(device))
-                        probs = torch.nn.functional.softmax(logits, dim=1).cpu().detach().numpy()
-                        preds = np.argmax(probs, 1)
-                        
-                        results[data_id, fold_id, transfer_id, epoch] = balanced_accuracy_score(y_test, preds)
-
-                    # np.save("results/transfer/comparison_imgnet_extraction", results)
-                    np.save("results/transfer/comparison_imgnet_finetuning", results)
+                np.save("results/transfer/comparison_imgnet_finetuning", results)
+                # np.save("results/transfer/comparison_wo_imgnet_finetuning", results)
